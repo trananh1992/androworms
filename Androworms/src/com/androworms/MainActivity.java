1ff2710f45ac97553a5251b3ebe60dbb84c6c379
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-11-12 19:54:30 +0000

    - Nouvelles icones - Splashscreen

--- Androworms/src/com/androworms/MainActivity.java
+++ Androworms/src/com/androworms/MainActivity.java
@@ -3,7 +3,9 @@ package com.androworms;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.text.Html;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -15,7 +17,17 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         
         /* Affiche la vue par défaut */
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
+        
+        /* Affiche la liste des développeurs */
+        String[] liste_dev = getResources().getStringArray(R.array.liste_developpeurs);
+        String txt_dev = "";
+        for (int i=0;i<liste_dev.length;i++) {
+        	txt_dev += liste_dev[i] + "<br/>";
+        }
+        TextView tv_developers;
+        tv_developers = (TextView)findViewById(R.id.textView3);
+        tv_developers.setText(Html.fromHtml(txt_dev));
     }
 
     @Override
