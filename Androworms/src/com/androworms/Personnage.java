0fbebbc9de0b77df1dea557567cd710684b2033e
vanhoutreve.renaud <vanhoutreve.renaud@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-09 12:41:07 +0000

    Mise en place des classes concernant les joueurs.

---
+++ Androworms/src/com/androworms/Personnage.java
@@ -0,0 +1,23 @@
+package com.androworms;
+
+/*
+ * Cette classe contient toutes les informations
+ * sur le personnage utiliser par un joueur.
+ */
+public class Personnage 
+{
+	/*
+	 * Propriete de personnage.
+	 * Image du personnage
+	 * couleur 
+	 * chapeau 
+	 * accessoire
+	 */
+	private Position position;
+	
+	public Personnage()
+	{
+		position = new Position();
+	}
+
+}
---
+++ Androworms/src/com/androworms/Position.java
@@ -0,0 +1,17 @@
+package com.androworms;
+
+/*
+ * Cette classe determine la position d'un objet/joueur sur une map
+ */
+public class Position 
+{
+	private int x;
+	private int y;
+	
+	public Position()
+	{
+		x = 0;
+		y = 0;
+	}
+
+}
