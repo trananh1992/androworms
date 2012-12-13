bc6360d529d31e439ebf0edbdb40c77f34a94131
vanhoutreve.renaud <vanhoutreve.renaud@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-13 21:21:10 +0000

    Ajout des munitions dans le dépot et définition des fonctions dans les classes Arme/Projectile/Munition.

---
+++ Androworms/src/com/androworms/Munition.java
@@ -0,0 +1,51 @@
+package com.androworms;
+
+public class Munition {
+	
+	private Projectile projectile;
+	private int nombre;
+	
+	public Munition(Projectile p)
+	{
+		this.projectile = p;
+		nombre = 0;
+	}
+	
+	public Projectile getProjectile() {
+		return projectile;
+	}
+	public void setProjectile(Projectile projectile) {
+		this.projectile = projectile;
+	}
+	public int getNombre() {
+		return nombre;
+	}
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+	
+	public boolean aEncoreProjectile()
+	{
+		if(nombre == 0) return false;
+		else return true;
+	}
+	
+	public Projectile prendreUnProjectile()
+	{
+		if(nombre > 0) nombre--;
+		return projectile;
+	}
+	
+	public void ajouter(int valeur)
+	{
+		if( nombre >= 0) 
+			nombre += valeur;
+	}
+	
+	public void projectileInfini()
+	{
+		nombre = -1;
+	}
+	
+
+}
