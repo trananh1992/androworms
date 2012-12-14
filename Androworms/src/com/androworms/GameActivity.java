e58e83663fd33e5152f8b322abf03725377fd79d
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-14 10:40:51 +0000

    - Test du slider dans l'autre sens - Corrections Sonar - Boutons sur le terrain de jeu

--- Androworms/src/com/androworms/GameActivity.java
+++ Androworms/src/com/androworms/GameActivity.java
@@ -1,7 +1,5 @@
 package com.androworms;
 
-import com.androworms.ui.TouchRelativeLayout;
-
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -11,6 +9,9 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
+
+import com.androworms.ui.Paddle;
+import com.androworms.ui.TouchRelativeLayout;
 
 public class GameActivity extends Activity {
 	
@@ -29,53 +30,58 @@ public class GameActivity extends Activity {
 		
 		
 		/** Intialisations des composants graphiques **/
-		RelativeLayout rl_globale = new RelativeLayout(this);
-			RelativeLayout rl_commandes = new RelativeLayout(this);
-				TextView txt_score = new TextView(this);
-				Button btn_cmd = new Button(this);
-			TouchRelativeLayout rl_carte = new TouchRelativeLayout(this);
-				ImageView imgv_fond = new ImageView(this);
-				ImageView imgv_jeu = new ImageView(this);
-				ImageView imgv_bonhomme = new ImageView(this);
+		RelativeLayout rlGlobale = new RelativeLayout(this);
+			RelativeLayout rlCommandes = new RelativeLayout(this);
+				TextView txtScore = new TextView(this);
+				Button btnArmes = new Button(this);
+				Paddle paddle = new Paddle(this);
+			TouchRelativeLayout rlCarte = new TouchRelativeLayout(this);
+				ImageView imgvFond = new ImageView(this);
+				ImageView imgvJeu = new ImageView(this);
+				ImageView imgvJoueur1 = new ImageView(this);
+		
 		
 		/** Paramètrages des composants graphiques **/
-		txt_score.setText("Score : 0");
-		btn_cmd.setText("bombe");
-		imgv_fond.setImageResource(R.drawable.image_fond_640x360);
-		imgv_fond.setId(40);
-		imgv_jeu.setImageResource(R.drawable.terrain_jeu_defaut_640x360);
-		imgv_jeu.setId(41);
-		imgv_bonhomme.setImageResource(R.drawable.logo_android_robot);
-		imgv_bonhomme.setId(42);
+		txtScore.setText("Score : 0");
+		btnArmes.setText("bombe");
 		
+		imgvFond.setImageResource(R.drawable.image_fond_640x360);
+		imgvJeu.setImageResource(R.drawable.terrain_jeu_defaut_640x360);
+		imgvJoueur1.setImageResource(R.drawable.logo_android_robot);
 		
 		/** Paramètrages des composants graphiques dans les Layout**/
-		LayoutParams params_txt_score = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		params_txt_score.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		params_txt_score.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-		params_txt_score.rightMargin = 40;
-		params_txt_score.topMargin = 40;
+		LayoutParams paramsTxtScore = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		paramsTxtScore.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		paramsTxtScore.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		paramsTxtScore.rightMargin = 40;
+		paramsTxtScore.topMargin = 40;
 		
-		LayoutParams params_btn_cmd = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		params_btn_cmd.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		params_btn_cmd.addRule(RelativeLayout.CENTER_VERTICAL);
+		LayoutParams paramsBtnArmes = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		paramsBtnArmes.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		paramsBtnArmes.addRule(RelativeLayout.CENTER_VERTICAL);
 		
-		LayoutParams params_imgv_bonhomme = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		params_imgv_bonhomme.addRule(RelativeLayout.CENTER_HORIZONTAL);
-		params_imgv_bonhomme.addRule(RelativeLayout.CENTER_VERTICAL);
+		LayoutParams paramsImgvJoueur1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		paramsImgvJoueur1.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		paramsImgvJoueur1.addRule(RelativeLayout.CENTER_VERTICAL);
+		
+		LayoutParams paramsPaddle = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		paramsPaddle.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		paramsPaddle.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		
 		
 		/** Construction de l'interface **/
-		setContentView(rl_globale);
-			rl_globale.addView(rl_carte);
-				rl_carte.addView(imgv_fond);
-				rl_carte.addView(imgv_jeu);
-				rl_carte.addView(imgv_bonhomme,params_imgv_bonhomme);
-			rl_globale.addView(rl_commandes);
-				rl_commandes.addView(txt_score,params_txt_score);
-				rl_commandes.addView(btn_cmd,params_btn_cmd);
+		setContentView(rlGlobale);
+			rlGlobale.addView(rlCarte);
+				rlCarte.addView(imgvFond);
+				rlCarte.addView(imgvJeu);
+				rlCarte.addView(imgvJoueur1,paramsImgvJoueur1);
+			rlGlobale.addView(rlCommandes);
+				rlCommandes.addView(txtScore,paramsTxtScore);
+				rlCommandes.addView(btnArmes,paramsBtnArmes);
+				rlCommandes.addView(paddle,paramsPaddle);
 		
 		/** Actions post-construction **/
-		rl_carte.init(this);
+		rlCarte.init(this);
 		
 		
 		// Solution 1 : je MATH_PARENT (donc image en taille max de l'écan et je peux le déplacer avec la matrice (sans "déborder de la taille du bonhmme)
---
+++ Androworms/src/com/androworms/ui/Paddle.java
@@ -0,0 +1,70 @@
+package com.androworms.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.androworms.R;
+
+/** Paddle de boutons pour gérer le déplacement
+ *
+ */
+public class Paddle extends LinearLayout implements OnTouchListener {
+	
+	public Paddle(Context context) {
+		super(context);
+		constructeurPartage(context);
+	}
+	
+	public Paddle(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		constructeurPartage(context);
+	}
+	
+	private void constructeurPartage(Context context) {
+		ImageButton btnGauche = new ImageButton(context);
+		ImageButton btnHaut = new ImageButton(context);
+		ImageButton btnDroite = new ImageButton(context);
+		
+		btnGauche.setImageResource(R.drawable.navigation_gauche);
+		btnHaut.setImageResource(R.drawable.navigation_haut);
+		btnDroite.setImageResource(R.drawable.navigation_droite);
+		
+		btnGauche.setOnTouchListener(this);
+		btnHaut.setOnTouchListener(this);
+		btnDroite.setOnTouchListener(this);
+		
+		LayoutParams paramsLayout = new LinearLayout.LayoutParams(64, 64);
+		
+		this.addView(btnGauche,paramsLayout);
+		this.addView(btnHaut,paramsLayout);
+		this.addView(btnDroite,paramsLayout);
+	}
+	
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				((ImageView)v).setAlpha(128);
+				break;
+		   case MotionEvent.ACTION_MOVE:
+				// si on bouge et qu'on se retrouve au-dessu du bouton
+				if(event.getX() > 0 && event.getX() < v.getWidth() && event.getY() > 0 && event.getY() < v.getHeight()) {
+					((ImageView)v).setAlpha(128);
+				}
+				else {
+					((ImageView)v).setAlpha(255);
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+				((ImageView)v).setAlpha(255);
+			default:
+				break;
+		}
+		return true;
+	}
+}
