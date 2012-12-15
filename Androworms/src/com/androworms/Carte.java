bd939a1fac196a39bc5369728b129239f1e7d8cd
william.bon2 <william.bon2@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-15 13:38:15 +0000

    SONAR : corrections

--- Androworms/src/com/androworms/Carte.java
+++ Androworms/src/com/androworms/Carte.java
@@ -11,16 +11,18 @@ import java.io.IOException;
 
 public class Carte {
 	private static final String TAG = "Androworms.Carte.Evenements";
-	private Bitmap b;
 	private Bitmap transformed;
 	public Carte(byte[] data) {
-		b = BitmapFactory.decodeByteArray(data, 0, data.length);
+		Bitmap b = BitmapFactory.decodeByteArray(data, 0, data.length);
 		
 		int height = b.getHeight();
 		int width = b.getWidth();
 		transformed = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
 		int i,j;
 		int color;
+		int seuil = 200;
+		int nComponents = 3;
+		int maxComponentValue = 255;
 		Log.v(TAG, "begin computing alpha");
 		for(i=0;i<width;i++)
 		{
@@ -30,13 +32,13 @@ public class Carte {
 				int blue = Color.blue(color);
 				int red = Color.red(color);
 				int green = Color.green(color);
-				if(((red + green + blue)/3)>200)
+				if(((red + green + blue)/nComponents)>seuil)
 				{
 					transformed.setPixel(i, j, Color.argb(0,red,green,blue));
 				}
 				else
 				{
-					transformed.setPixel(i, j, Color.argb(255,red,green,blue));
+					transformed.setPixel(i, j, Color.argb(maxComponentValue,red,green,blue));
 				}
 			}
 		}
@@ -44,15 +46,24 @@ public class Carte {
 	}
 	
 	public void save(String path) {
-		FileOutputStream s;
+		FileOutputStream s = null;
+		int compression = 100;
 		try {
 			s = new FileOutputStream(path);
-			transformed.compress(Bitmap.CompressFormat.PNG, 100, s);
+			transformed.compress(Bitmap.CompressFormat.PNG, compression, s);
 			s.flush();
 			s.close();
 		} catch (FileNotFoundException e) {
 			Log.e(TAG,"File not found for saving");
 		} catch (IOException e) {
+			try
+			{
+				if(s != null)
+					s.close();
+			}
+			catch (IOException e2)
+			{
+			}
 			Log.e(TAG,"IO Exception in save");
 		}
 	}
