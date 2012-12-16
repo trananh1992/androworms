7d23174ee2ae5e33c2b7e515bf204ceece47a65a
william.bon2 <william.bon2@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-16 15:20:59 +0000

    Ajout d'une activité pour l'édition des cartes correction d'un conflit générés par correction sonar -_-

---
+++ Androworms/src/com/androworms/ActiviteCreationCarte.java
@@ -0,0 +1,61 @@
+package com.androworms;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+
+public class ActiviteCreationCarte extends Activity implements OnClickListener,OnTouchListener {
+	private static final String TAG = "Androworms.ActiviteCreationCarte.Event";
+	private MenuPrincipalActivity activiteMenuPrincipal;
+	static final int TAKE_PICTURE = 0;
+	
+	public void onClick(View arg0) {
+		Log.v(TAG,"On a cliqué sur l'activité de creation de carte");
+		Intent intent = new Intent(this.activiteMenuPrincipal, ActiviteCreationCarte.class);
+		Log.v(TAG,"Androworms : created intent!");
+		this.activiteMenuPrincipal.startActivityForResult(intent,TAKE_PICTURE);
+		Log.v(TAG,"Androworms : started activity!");
+		
+	}
+	
+	public ActiviteCreationCarte(MenuPrincipalActivity activiteMenuPrincipal) {
+		this.activiteMenuPrincipal = activiteMenuPrincipal;
+	}
+	
+	public ActiviteCreationCarte() {
+		super();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/* Affiche la vue par défaut */
+		setContentView(R.layout.edition_carte);
+		
+		OnClickListener camCl = new ActiviteCamera(this);
+		//	OnTouchListener camtl = new ActiviteCreationCarte(this);
+		findViewById(R.id.TakePicture).setOnClickListener(camCl);
+	}
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent arg1) {
+		// TODO Auto-generated method stub
+		Log.v(TAG,"touch me");
+		return false;
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode,
+            Intent data) {
+        if (requestCode == TAKE_PICTURE) {
+            if (resultCode == RESULT_OK) {
+                
+            }
+        }
+    }
+
+}
