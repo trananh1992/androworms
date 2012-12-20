9d76ccb68e6abca17353a0f2c1551d23104bd1f6
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2012-12-20 15:27:36 +0000

    Version bêta de la gestion du tir

--- trunk/Androworms/src/com/androworms/GameActivity.java
+++ trunk/Androworms/src/com/androworms/GameActivity.java
@@ -3,12 +3,22 @@ package com.androworms;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.androworms.ui.TouchRelativeLayout;
 
 public class GameActivity extends Activity {
 	
 	private static final String TAG = "TESTAndroworms.MainActivity";
+	
+	/* Etats possible pour le mode de gestion des doigts */
+	public static int mode;
+	public static final int RIEN = 0;
+	public static final int DEPLACEMENT = 1;
+	public static final int ZOOM = 2;
+	public static final int TIR = 3;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +29,17 @@ public class GameActivity extends Activity {
 		
 		/* Récupération du layout de fond */
 		TouchRelativeLayout trl = (TouchRelativeLayout)findViewById(R.id.trlCarte);
+		
+		Button btnTir = (Button)findViewById(R.id.button1);
+		btnTir.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				if (mode == TIR) {
+					mode = RIEN;
+				} else {
+					mode = TIR;
+				}
+			}
+		});
 		
 		/** Actions post-construction **/
 		trl.init(this);
