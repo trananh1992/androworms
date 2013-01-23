26559f5df6564840d08f36397a39610ff5106cf0
vanhoutreve.renaud <vanhoutreve.renaud@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-01-23 18:03:20 +0000

    La sortie du personnage de l'écran vers le bas provoquant une erreur est réglé. De même pour la collision entre deux joueurs.

--- trunk/Androworms/src/com/androworms/Personnage.java
+++ trunk/Androworms/src/com/androworms/Personnage.java
@@ -1,5 +1,8 @@
 package com.androworms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.graphics.PointF;
 
 /*
@@ -18,10 +21,39 @@ public class Personnage extends ElementSurCarte {
 
 	private String nom;
 
+
 	
 	public Personnage(String nom, ImageInformation ii) {
 		super(new PointF(), ii);
 		this.nom = nom;
+	}
+	
+	public List<PointF> getEnveloppeConvexeHaut() {
+		List<PointF> result = new ArrayList<PointF>();
+		result.add(getPosition());
+		result.add(new PointF(getPosition().x+getWidthImageTerrain(), getPosition().y));
+		return result;
+	}
+	
+	public List<PointF> getEnveloppeConvexeBas() {
+		List<PointF> result = new ArrayList<PointF>();
+		result.add(new PointF(getPosition().x+getWidthImageTerrain(), getPosition().y+getHeightImageTerrain()));
+		result.add(new PointF(getPosition().x, getPosition().y+getHeightImageTerrain()));
+		return result;
+	}
+	
+	public List<PointF> getEnveloppeConvexeDroite() {
+		List<PointF> result = new ArrayList<PointF>();
+		result.add(new PointF(getPosition().x+getWidthImageTerrain(), getPosition().y));
+		result.add(new PointF(getPosition().x+getWidthImageTerrain(), getPosition().y+getHeightImageTerrain()));
+		return result;
+	}
+	
+	public List<PointF> getEnveloppeConvexeGauche() {
+		List<PointF> result = new ArrayList<PointF>();
+		result.add(getPosition());
+		result.add(new PointF(getPosition().x, getPosition().y+getHeightImageTerrain()));
+		return result;
 	}
 
 	public String getNom() {
@@ -53,5 +85,66 @@ public class Personnage extends ElementSurCarte {
 			setPosition(getPosition().x - pas, getPosition().y);
 		}
 
+	}
+	
+	
+	// Test si le personnage courant est en dessous du personnage p.
+	public boolean estEnDessous(Personnage p) {
+		List<PointF> reference = getEnveloppeConvexeHaut();
+		List<PointF> comparateur = p.getEnveloppeConvexeBas();
+		for(int i = 0; i < reference.size(); i++) {
+			for(int j= 0; j < comparateur.size(); j++) {
+				if(reference.get(i).y < comparateur.get(i).y) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	// Test si le personnage courant est en dessus du personnage p.
+	public boolean estAuDessus(Personnage p) {
+		List<PointF> reference = getEnveloppeConvexeBas();
+		List<PointF> comparateur = p.getEnveloppeConvexeHaut();
+		for(int i = 0; i < reference.size(); i++) {
+			for(int j= 0; j < comparateur.size(); j++) {
+				if(reference.get(i).y > comparateur.get(i).y) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	//Test si le personnage courant est a gauche du personnage p.
+	public boolean estAGauche(Personnage p) {
+		List<PointF> reference = getEnveloppeConvexeDroite();
+		List<PointF> comparateur = p.getEnveloppeConvexeGauche();
+		for(int i = 0; i < reference.size(); i++) {
+			for(int j= 0; j < comparateur.size(); j++) {
+				if(reference.get(i).x > comparateur.get(i).x) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	//Test si le personnage courant est a droite du personnage p.
+	public boolean estADroite(Personnage p) {
+		List<PointF> reference = getEnveloppeConvexeGauche();
+		List<PointF> comparateur = p.getEnveloppeConvexeDroite();
+		for(int i = 0; i < reference.size(); i++) {
+			for(int j= 0; j < comparateur.size(); j++) {
+				if(reference.get(i).x < comparateur.get(i).x) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	// Test si le personnage est en collision avec un autre.
+	public boolean estEnCollision(Personnage p) {
+		return !(estEnDessous(p) || estAuDessus(p) || estADroite(p) || estAGauche(p));
 	}
 }
