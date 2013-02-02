9c744d9563108fc0b4f6a11d10b1f60e7cb0ae1a
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-02-02 17:31:34 +0000

    Modification du menu principale : - Uniformisation de nommage des composants - Refonte du menu principale - Ajout d'icônes

--- Androworms/src/com/androworms/ActiviteAndrowormsEvent.java
+++ Androworms/src/com/androworms/ActiviteAndrowormsEvent.java
@@ -1,8 +1,5 @@
 package com.androworms;
 
-import com.androworms.debug.ActiviteDebug;
-import com.androworms.multijoueurs.ActiviteMultiJoueur;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.util.Log;
@@ -10,9 +7,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.androworms.debug.ActiviteDebug;
+import com.androworms.multijoueurs.ActiviteMultiJoueur;
+
 public class ActiviteAndrowormsEvent implements OnClickListener {
 	
-	private static final String TAG = "Androworms.MenuPrincipalActivityEvent";
+	private static final String TAG = "Androworms.ActiviteAndrowormsEvent";
 	
 	private ActiviteAndroworms activiteAndroworms;
 	
@@ -23,37 +23,45 @@ public class ActiviteAndrowormsEvent implements OnClickListener {
 	public void onClick(View arg0) {
 		if (arg0 instanceof Button) {
 			Button b = (Button)arg0;
-			
-			if (b.getId() == R.id.btn_solo) {
-				// Bouton de jeu en mode solo
-				Log.v(TAG,"Lancement du jeu");
-				
+			if (b.getId() == R.id.btn_menu_jouer) {
+				// Bouton pour jouer
+				Log.v(TAG, "Clic sur le bouton pour jouer");
 				Intent intent = new Intent(this.activiteAndroworms, ActiviteJeu.class);
 				this.activiteAndroworms.startActivity(intent);
 			}
-			else if (b.getId() == R.id.btn_multi) {
-				//Bouton de jeu en mode multi-joueur
-				
-				Intent intent = new Intent(this.activiteAndroworms, ActiviteMultiJoueur.class);
+			else if (b.getId() == R.id.btn_menu_editeur) {
+				// Bouton de l'éditeur de carte
+				Log.v(TAG, "Clic sur le bouton de l'éditeur de carte");
+				Intent intent = new Intent(this.activiteAndroworms, ActiviteChoixOption.class);
 				this.activiteAndroworms.startActivity(intent);
 			}
 			else if (b.getId() == R.id.btn_menu_score) {
 				// Bouton de gestion des scores
+				Log.v(TAG, "Clic sur le bouton de la gestion des scores");
 				new AlertDialog.Builder(activiteAndroworms).setTitle("Androworms").setMessage("La gestion des scores n'est pas encore disponible dans cette version").setNeutralButton("Close", null).show();
 			}
-			else if (b.getId() == R.id.btn_menu_settings) {
+			else if (b.getId() == R.id.btn_menu_parametres) {
 				// Bouton de paramètres
+				Log.v(TAG, "Clic sur le bouton des paramètres");
 				Intent intent = new Intent(this.activiteAndroworms, ActiviteParametres.class);
 				this.activiteAndroworms.startActivity(intent);
 			}
 			else if (b.getId() == R.id.btn_DEBUG) {
 				// Bouton de DEBUG
+				Log.v(TAG, "Clic sur le bouton de DEBUG");
 				Intent intent = new Intent(this.activiteAndroworms, ActiviteDebug.class);
 				this.activiteAndroworms.startActivity(intent);
 			}
 			else if (b.getId() == R.id.btn_GYRO) {
 				// Bouton de Gyro
+				Log.v(TAG, "Clic sur le bouton du Gyroscope");
 				Intent intent = new Intent(this.activiteAndroworms, ActiviteGyro.class);
+				this.activiteAndroworms.startActivity(intent);
+			}
+			else if (b.getId() == R.id.btn_menu_multi) {
+				// Bouton de l'éditeur de carte
+				Log.v(TAG, "Clic sur le bouton du mode multi-joueurs");
+				Intent intent = new Intent(this.activiteAndroworms, ActiviteMultiJoueur.class);
 				this.activiteAndroworms.startActivity(intent);
 			}
 		}
