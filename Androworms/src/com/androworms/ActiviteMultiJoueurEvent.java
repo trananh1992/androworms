109f884fb25b376f2bab52633af1fffd29d73f61
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-02-04 10:07:40 +0000

    Divers fix et améliorations : - Corrections violations Sonar - Ajout de texte sur le MultiJoueurs Bluetooth - Améliorations de l'activité de création de partie - Début de la fusion entre l'activité MultiJoueur et l'activité de Création de partie

--- Androworms/src/com/androworms/ActiviteMultiJoueurEvent.java
+++ Androworms/src/com/androworms/ActiviteMultiJoueurEvent.java
@@ -4,9 +4,8 @@ import android.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.widget.Button;
 
-import com.androworms.R;
 import com.androworms.utile.Informations;
 
 public class ActiviteMultiJoueurEvent implements OnClickListener {
@@ -19,10 +18,10 @@ public class ActiviteMultiJoueurEvent implements OnClickListener {
 	}
 
 	public void onClick(View v) {
-		if (v instanceof ImageButton) {
-			ImageButton imgbtn = (ImageButton)v;
+		if (v instanceof Button) {
+			Button btn = (Button)v;
 			
-			if (imgbtn == activiteMultiJoueur.findViewById(R.id.imgbtn_bluetooth)) {
+			if (btn == activiteMultiJoueur.findViewById(R.id.btn_bluetooth)) {
 				/* Choix du Bluetooth */
 				if (!Informations.isCompatibleBluetooth()) {
 					// Le téléphone n'est pas compatible Blueooth
@@ -34,11 +33,11 @@ public class ActiviteMultiJoueurEvent implements OnClickListener {
 					activiteMultiJoueur.changerVue(this);
 				}
 			}
-			else if (imgbtn == activiteMultiJoueur.findViewById(R.id.imgbtn_deux_joueurs)) {
+			else if (btn == activiteMultiJoueur.findViewById(R.id.btn_deux_joueurs)) {
 				/* Choix du mode 2 joueurs */
 				new AlertDialog.Builder(activiteMultiJoueur).setTitle("Androworms").setMessage("Les parties multi-joueurs ne sont pas encore dispo !").setNeutralButton("Close", null).show();
 			}
-			else if (imgbtn == activiteMultiJoueur.findViewById(R.id.imgbtn_bluetooth_serveur)) {
+			else if (btn == activiteMultiJoueur.findViewById(R.id.btn_bluetooth_serveur)) {
 				/* Choix du Bluetooth > Serveur */
 				activiteMultiJoueur.isServeur = true;
 				
@@ -50,7 +49,7 @@ public class ActiviteMultiJoueurEvent implements OnClickListener {
 				/* Actualisation des elements graphiques *///DELETE ?
 				//activiteMultiJoueur.getFonctions_IHM().refresh_UI_multijoueur_bluetooth_serveur();
 			}
-			else if (imgbtn == activiteMultiJoueur.findViewById(R.id.imgbtn_bluetooth_client)) {
+			else if (btn == activiteMultiJoueur.findViewById(R.id.btn_bluetooth_client)) {
 				/* Choix du Bluetooth > Client */
 				Log.v(TAG, "Choix du mode Bluetooth > Client");
 				activiteMultiJoueur.isServeur = false;
