6d0ee358bf21b01a9cc5adb18e2b2b7b3e592209
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-02-15 22:20:00 +0000

    Amélioration de l'ActiviteParametres de l'application

--- Androworms/src/com/androworms/ActiviteParametres.java
+++ Androworms/src/com/androworms/ActiviteParametres.java
@@ -11,7 +11,16 @@ import android.widget.EditText;
 
 public class ActiviteParametres extends Activity {
 	
-	public static final String PREFS_NAME = "Parametres";
+	// Constante de nom du fichier de préférences
+	public static final String PARAMETRES_CLE = "Parametres";
+	// Constantes de clé et de valeurs par défaut des préférences
+	public static final String PARAMETRE_PSEUDO_CLE = "Pseudo";
+	public static final String PARAMETRE_PSEUDO_DEFAUT_1 = "";
+	public static final String PARAMETRE_PSEUDO_DEFAUT_2 = "Joueur 1";
+	public static final String PARAMETRE_VIBRATIONS_CLE = "vibrations";
+	public static final boolean PARAMETRE_VIBRATIONS_DEFAUT = false;
+	public static final String PARAMETRE_SONS_CLE = "Sons";
+	public static final boolean PARAMETRE_SONS_DEFAUT = true;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +28,18 @@ public class ActiviteParametres extends Activity {
 		setContentView(R.layout.activite_parametres);
 		
 		// Chargement des préférences
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		String pseudo = settings.getString("pseudo", "");
-		boolean vibrations = settings.getBoolean("vibrations", false);
+		SharedPreferences settings = getSharedPreferences(PARAMETRES_CLE, 0);
+		String paramPseudo = settings.getString(PARAMETRE_PSEUDO_CLE, PARAMETRE_PSEUDO_DEFAUT_1);
+		boolean paramVibrations = settings.getBoolean(PARAMETRE_VIBRATIONS_CLE, PARAMETRE_VIBRATIONS_DEFAUT);
+		boolean paramSons = settings.getBoolean(PARAMETRE_SONS_CLE, PARAMETRE_SONS_DEFAUT);
 		
 		// Configuration des composants
 		EditText etPseudo = (EditText)findViewById(R.id.et_pseudo);
-		etPseudo.setText(pseudo);
+		etPseudo.setText(paramPseudo);
 		CheckBox chkVibrations = (CheckBox)findViewById(R.id.chk_vibrations);
-		chkVibrations.setChecked(vibrations);
+		chkVibrations.setChecked(paramVibrations);
+		CheckBox chkSons = (CheckBox)findViewById(R.id.chk_sons);
+		chkSons.setChecked(paramSons);
 		
 		/* Bouton enregistrer */
 		Button btn = (Button)findViewById(R.id.btn_enregistrer);
@@ -43,11 +55,13 @@ public class ActiviteParametres extends Activity {
 		// Sauvegarde des préférences
 		EditText etPseudo = (EditText)findViewById(R.id.et_pseudo);
 		CheckBox chkVibrations = (CheckBox)findViewById(R.id.chk_vibrations);
+		CheckBox chkSons = (CheckBox)findViewById(R.id.chk_sons);
 		
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences settings = getSharedPreferences(PARAMETRES_CLE, 0);
 		SharedPreferences.Editor editor = settings.edit();
-		editor.putString("pseudo", etPseudo.getText().toString());
-		editor.putBoolean("vibrations", chkVibrations.isChecked());
+		editor.putString(PARAMETRE_PSEUDO_CLE, etPseudo.getText().toString());
+		editor.putBoolean(PARAMETRE_VIBRATIONS_CLE, chkVibrations.isChecked());
+		editor.putBoolean(PARAMETRE_SONS_CLE, chkSons.isChecked());
 		editor.commit();
 	}
 }
