55b4889a8f7096286eb15042443252f42a1acd24
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-06-09 06:07:07 +0000

    Amélioration du partage de l'application : - Ajout du droit d'accéder aux comptes pour le partage sur Google+ - Ajout des traductions françaises du bouton Facebook - Ajout des images du Play Store

--- Androworms/src/com/androworms/ActivitePartage.java
+++ Androworms/src/com/androworms/ActivitePartage.java
@@ -1,23 +1,44 @@
 package com.androworms;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class ActivitePartage extends Activity {
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.PlusClient;
+
+public class ActivitePartage extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
 	
 	private static final String TAG = "Partage";
+	
+	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+	
+	private ProgressDialog mConnectionProgressDialog;
+	private PlusClient mPlusClient;
+	private ConnectionResult mConnectionResult;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activite_partage);
 		
-		Log.v(TAG, "Activité de partage de l'application");
+		
 		
 		// Bouton "partager"
 		ImageButton ibPartagerGeneral = (ImageButton)findViewById(R.id.ib_partager_general);
@@ -45,13 +66,125 @@ public class ActivitePartage extends Activity {
 				// On démarre le sélécteur d'applications pour le partage
 				startActivity(Intent.createChooser(shareIntent, "Partager l'application"));
 			}
-			
 		});
 		
-		// Bouton "Facebook"
-		// TODO
+		// Bouton "Evaluer sur le Play Store"
+		Button btnEvaluerPlayStore = (Button)findViewById(R.id.btn_play_store);
+		btnEvaluerPlayStore.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String url = "market://details?id=com.androworms";
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				startActivity(i);
+			}
+		});
+		
+		
+		
+		// Bouton "Facebook-Login"
+		com.facebook.widget.LoginButton btnLoginFacebook = (com.facebook.widget.LoginButton)findViewById(R.id.btn_login_facebook);
+		btnLoginFacebook.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				loginFacebook();
+			}
+		});
+		
 		
 		// Bouton "Google+"
-		// TODO
+		com.google.android.gms.common.SignInButton btnLoginGoogle = (com.google.android.gms.common.SignInButton)findViewById(R.id.btn_login_google);
+		btnLoginGoogle.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				mPlusClient.connect();
+			}
+		});
+		
+		mPlusClient = new PlusClient.Builder(this, this, this)
+		.setVisibleActivities("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+		.build();
+		// Barre de progression à afficher si l'échec de connexion n'est pas résolu.
+		mConnectionProgressDialog = new ProgressDialog(this);
+		mConnectionProgressDialog.setMessage("Connexion...");
+	}
+	
+	
+	public void loginFacebook() {
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+			
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				Log.v(TAG, "call()");
+				if (session.isOpened()) {
+					Log.v(TAG, "session.isOpened()");
+					
+					// make request to the /me API
+					Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+						
+						// callback after Graph API response with user object
+						@Override
+						public void onCompleted(GraphUser user, Response response) {
+							Log.v(TAG, "onCompleted()");
+							if (user != null) {
+								Log.v(TAG, "user != null");
+								//TextView welcome = (TextView) findViewById(R.id.welcome);
+								//welcome.setText("Hello " + user.getName() + "!");
+								Button ibPartagerFacebook = (Button) findViewById(R.id.btn_partager_facebook);
+								ibPartagerFacebook.setText("Hello " + user.getName() + "!");
+							}
+						}
+					});
+				}
+			}
+		});
+	}
+	
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mPlusClient.disconnect();
+	}
+	
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (result.hasResolution()) {
+			try {
+				result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+			} catch (SendIntentException e) {
+				mPlusClient.connect();
+			}
+		}
+		// Enregistrer le résultat et résoudre l'échec de connexion lorsque l'utilisateur clique.
+		mConnectionResult = result;
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+		super.onActivityResult(requestCode, responseCode, intent);
+		if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
+			mConnectionResult = null;
+			mPlusClient.connect();
+		}
+		else {
+			Session.getActiveSession().onActivityResult(this, requestCode, responseCode, intent);
+		}
+	}
+	
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		String accountName = mPlusClient.getAccountName();
+		Toast.makeText(this, accountName + " est connecté.", Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	public void onDisconnected() {
+		Log.d(TAG, "disconnected");
 	}
 }
