b8f61982edbcf47483058fb45b94220279cff1c7
damien.finck67 <damien.finck67@df5b815c-4ed9-ce81-8ae4-2fe3c79b497d>
2013-06-15 21:16:07 +0000

    Corrections violations Sonar

--- trunk/Androworms/src/com/androworms/ImageInformation.java
+++ trunk/Androworms/src/com/androworms/ImageInformation.java
@@ -37,40 +37,36 @@ public class ImageInformation {
 		this.height = height;
 		this.width = width;
 	}
-
+	
 	public int getWidth() {
 		return width;
 	}
-
+	
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
+	
 	public int getHeight() {
 		return height;
 	}
-
+	
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	
 	public int getIdImage() {
 		return idImage;
 	}
-
+	
 	public void setIdImage(int idImage) {
 		this.idImage = idImage;
 	}
-
+	
 	public Bitmap getImageView() {
 		return Bitmap.createScaledBitmap(imageView,(int)(getWidth()*echelle), (int)(getHeight()*echelle), true);
 	}
-
+	
 	public void setImageView(Bitmap imageView) {
 		this.imageView = imageView;
 	}
-	
-	
-	
-	
 }
